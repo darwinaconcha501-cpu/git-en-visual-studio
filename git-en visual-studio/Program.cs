0d4f479df683bed4ea60456516a2a9971bd4d2da
Darwin Balguera <Darwinaconcha501@gmail.com>
2026-09-24 19:50:10 -0500

    agregar metodo suma

--- git-en visual-studio/Program.cs
+++ git-en visual-studio/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+        }
+        static double Suma(double num1, double num2,double resultado) 
+        { 
+          resultado = num1 + num2;  
+          return resultado;
         }
     }
 }
