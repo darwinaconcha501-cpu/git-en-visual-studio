94d1bf45b38ff6cbd536638d137da1fe0ed637b6
Darwin Balguera <Darwinaconcha501@gmail.com>
2026-09-24 19:39:02 -0500

    Agregar archivos de proyecto.

---
+++ git-en visual-studio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace git_en_visual_studio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
